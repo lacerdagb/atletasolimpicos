b87c9a071ece937c42791ad8d4cd8b1c9a5964e3
Gabriel Lacerda <127674463+lacerdagb@users.noreply.github.com>
2024-09-07 18:38:55 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,24 @@
+let dados = [ // Sempre que for usar lista, tem que abrir os cochetes[] 
+    // Sempre que criar objetos tem que botar {} para cada 
+    {
+        titulo: "Rayssa Leal",
+        descricao: " Rayssa Leal, conhecida como Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento precoce e carisma. Aos poucos anos, já acumulava títulos importantes e se tornou um ícone do esporte feminino. Sua história inspiradora e suas manobras incríveis a tornaram uma das atletas mais admiradas do país.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "fada skate sk8"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Judoca brasileira que se destaca pela sua técnica apurada e determinação. Com diversas vitórias em competições nacionais e internacionais, ela é um dos grandes nomes do judô brasileiro.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judo judoka judô"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Ginasta artística brasileira que conquistou o coração dos brasileiros e se tornou um dos maiores nomes do esporte nacional. Com talento inegável e determinação, ela acumula diversas medalhas em competições nacionais e internacionais, incluindo ouro olímpico.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags:"ginástica ginastica ouro"
+    }
+];
+    
+
+    
